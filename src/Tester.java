5f5817870be96a98c39e8f16eec64a5681bb778e
Allan <allan.wei@mail.mcgill.ca>
2020-12-22 02:03:27 -0500

    Organized tester, and added indications as to how many tests have occured.

--- src/Tester.java
+++ src/Tester.java
@@ -7,10 +7,11 @@ import java.util.function.Function;
 
 public class Tester {
     public static void main(String[] args){
-        int size = 2;
-        int repeat = 1;
+        int size = 10000;
+        int repeat = 10000;
+        boolean print = false;
 
-        test(InsertionSort::sort, size, repeat);
+        test(InsertionSort::sort, size, repeat, print);
 
     }
     public static boolean testSorted(int[] array){
@@ -38,25 +39,44 @@ public class Tester {
         return sum.divide(BigDecimal.valueOf(array.size()));
     }
     public static void test(Function<int[], int[]> sort, int size, int repeat){
+        test(sort,size,repeat,false);
+    }
+    public static void test(Function<int[], int[]> sort, int size, int repeat, boolean print){
         Random random = new Random();
         ArrayList<Long> avg = new ArrayList<Long>();
         int[] data = {0};
 
-        //test if the sort changes the list
-        int[] numtest = random.ints(50, 1, 100).toArray();
+        //tests to see if the sort works
+        int[] numtest = random.ints(10, 1, 10).toArray();
         int[] numtest2 = numtest.clone();
         numtest2 = sort.apply(numtest2);
-        System.out.println(Arrays.toString(numtest));
-        System.out.println(Arrays.toString(numtest2));
+        if (print) {
+            System.out.println("Test 1 to see if the sort changes the elements: " + Arrays.toString(numtest));
+            System.out.println("Test 2 to see if the sort changes the elements: " + Arrays.toString(numtest2));
+        }
         if (!sameList(numtest,numtest2)){
             System.out.println("The sort changed the list. Test Cancelled");
             return;
         }
+        if (print){
+            System.out.println("Input of the sort: " + Arrays.toString(numtest));
+            System.out.println("Output of the sort: " + Arrays.toString(numtest2));
+        }
+        if (!testSorted(numtest2)){
+            System.out.println("The sort didn't correct sort in descending order. Test Cancelled");
+            System.out.println("Input of the sort: " + Arrays.toString(numtest));
+            System.out.println("Output of the sort: " + Arrays.toString(numtest2));
+            return;
+        }
 
         //main test for sort
-        for (int i = 0; i < repeat; i++) {
+        for (int i = 1; i <= repeat; i++) {
+            int j = 100;
+            if (i % 100 == 0){
+                System.out.println(j + " tests completed.");
+                j += 100;
+            }
             data = random.ints(size, 1, size * 2).toArray();
-            //System.out.println(Arrays.toString(data));
             long startTime = System.nanoTime();
             data = sort.apply(data);
             long endTime = System.nanoTime();
@@ -64,10 +84,7 @@ public class Tester {
             avg.add(duration);
         }
 
-        //System.out.println(Arrays.toString(data));
         BigDecimal time = average(avg);
-
-        System.out.println("Did the sort work? " + testSorted(data));
         //divide by 1000000 to get milliseconds.
         System.out.println("Sort took " + time.divide(BigDecimal.valueOf(1000000)) + " ms on average of " + repeat + " attempts for a list of size " + size + ".");
     }
